7ac9bd3c8d9b98190f154f539d74f694b792f77d
Matan22g <64740256+Matan22g@users.noreply.github.com>
2020-12-09 21:14:50 +0200

    showing graphs, caculate error, shows acc in each group. total 92 percent accuracy

--- class_algo.py
+++ class_algo.py
@@ -4,13 +4,13 @@ import sys
 import matplotlib.pylab as plt
 import numpy as np
 
-
 def progressBar(i, total, percision):
     percent = (i / total) * 100
     sys.stdout.write("\rIter Done %i/%i, Percision %i%%" % (i, total, percision))
     if i == total - 1:
         print("\n")
     sys.stdout.flush()
+
 
 def my_softmax(x):
     # Compute softmax values for each row in matrix x
@@ -32,17 +32,17 @@ def predict(im, W):
 # getting W matrix of 10X(img_dim^2) and X is vector of training data
 # t is the real result of the training
 # then calc the E = SUM(SUM(t_n_k*ln(y_n_k))
-def calc_loss_entropy_cross(W, t, X, N):
+
+def calc_loss_entropy_cross(y_nk, t):
+    N = t.shape[0]
     E_w = 0
-    W_xn_mat = X @ W.T
-    y_n_k = my_softmax(W_xn_mat)
-    E_w += np.log(y_n_k) - t
-    return E_w
+    for n in range(N):
+        for k in range(10):
+            E_w += t[n][k] * y_nk[n][k]
+    return -E_w
 
 
-def update_weights(W, h, t, X, N):
-    W_xn_mat = X @ W.T
-    y_nk = my_softmax(W_xn_mat)
+def update_weights(y_nk, X, W, t, h):
     grad = X.T @ (y_nk - t)
     W = W - (h * grad).T
     return W
@@ -61,8 +61,11 @@ def calc_percision(X_valid, t_valid, W):
 
     return percision
 
+
+#### Method to run it all
+
 def classiffy(data, heta, iter):
-    print("Start classifiy")
+    # print("Start classifiy")
     X_train, X_test, X_valid, t_train, t_test, t_valid = data
     h = heta  # heta
     N = X_train.shape[0]  # Number of samples to train
@@ -77,7 +80,7 @@ def classiffy(data, heta, iter):
     percision = 0
     num_of_consistency = 0
     max_val = np.amax(X_train)
-    print("Heta:", h)
+    # print("Heta:", h)
 
     # Convert str to int
     t_train_int = t_train.astype(np.int)
@@ -94,18 +97,20 @@ def classiffy(data, heta, iter):
     percision_array = []
     loss_array = []
     i = 0
-
     while True:
         i += 1
-
         progressBar(i, iter_lim, percision)
-        W = update_weights(W, h, t_train_mat, X_train, N)
 
-        loss = calc_loss_entropy_cross(W, t_train_mat, X_train, N)
+        W_xn_mat = X_train @ W.T
+        y_nk = my_softmax(W_xn_mat)
+
+        loss = calc_loss_entropy_cross(y_nk, t_train_mat)
         loss_array.append(loss)
 
         percision = calc_percision(X_valid, t_valid_mat, W)
         percision_array.append(percision)
+
+        W = update_weights(y_nk, X_train, W, t_train_mat, h)
 
         if last_percision == 0:
             last_percision = percision
@@ -124,23 +129,28 @@ def classiffy(data, heta, iter):
             print("\nencounter iter limit stoping..")
             break
 
+    print("heta: ", h)
+    print("iter: ", i)
+    print("percision: ", percision)
     return W, percision_array, loss_array
+
 
 def main_classiffy(data):
 
-    heta = 0.015
-    iteration = 300
+    heta = 1.3
+    iteration = 20
     W, percision_array, loss_array = classiffy(data, heta, iteration)
+
     X_train, X_test, X_valid, t_train, t_test, t_valid = data
 
     #### Further amusement
-    x1_for_plot = [i+1 for i in range(len(percision_array))]
-    x2_for_plot = [i+1 for i in range(len(percision_array))]
+    x1_for_plot = [i + 1 for i in range(len(percision_array))]
+    x2_for_plot = [i + 1 for i in range(len(loss_array))]
     fig = plt.figure()
     ax1 = fig.add_subplot(1, 2, 1)
     ax2 = fig.add_subplot(1, 2, 2)
     ax1.plot(x1_for_plot, percision_array, label='Percision Percantage')
-    ax2.plot(x2_for_plot, percision_array, label='loss_array')
+    ax2.plot(x2_for_plot, loss_array, label='loss_array')
     ax1.set_xlabel('Iteration (#)')
     ax1.set_ylabel('Percision (%)')
     ax1.set_title('Percision Percantage')
@@ -150,6 +160,32 @@ def main_classiffy(data):
     ax2.set_title('loss_array')
     ax2.legend()
     plt.show()
+
+
+    # Convert string to int
+    t_train_int = t_train.astype(np.int)
+    t_valid_int = t_valid.astype(np.int)
+    t_test_int = t_test.astype(np.int)
+
+    # Build t matrices
+    t_train_mat = np.zeros((t_train.shape[0], 10), dtype=int)
+    t_valid_mat = np.zeros((t_valid.shape[0], 10), dtype=int)
+    t_test_mat = np.zeros((t_test.shape[0], 10), dtype=int)
+
+    t_train_mat[[range(t_train.shape[0])], [t_train_int]] = 1
+    t_valid_mat[[range(t_valid.shape[0])], [t_valid_int]] = 1
+    t_test_mat[[range(t_test.shape[0])], [t_test_int]] = 1
+
+    X_train, X_test, X_valid = X_train / 255, X_test / 255, X_valid / 255
+
+    percision_t_test = calc_percision(X_train, t_train_mat, W)
+    percision_t_train = calc_percision(X_test, t_test_mat, W)
+    percision_t_valid = calc_percision(X_valid, t_valid_mat, W)
+
+    print("percision_t_train: ", percision_t_train)
+    print("percision_t_valid: ", percision_t_valid)
+    print("percision_t_test: ", percision_t_test)
+
 
 def test(W,x,t):
 
